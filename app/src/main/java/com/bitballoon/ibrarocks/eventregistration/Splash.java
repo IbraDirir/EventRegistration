6c928e5d35ac0b3484e6fc6f77f32024442b4f54
ibra-dirir <khaliildirir@gmail.com>
2015-10-27 06:13:46 -0700

    added new splash layout file

---
+++ app/src/main/java/com/bitballoon/ibrarocks/eventregistration/Splash.java
@@ -0,0 +1,14 @@
+package com.bitballoon.ibrarocks.eventregistration;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by dirir on 10/27/2015.
+ */
+public class Splash extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
